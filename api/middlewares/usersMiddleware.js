b6ab3ef0aac45bc7dadb6f9f5d086c88eaf03a90
Martin Twum Mensah <martint.mensah@gmail.com>
2019-07-22 13:31:12 +0000

    feature: Add middleware to reverse password hash

--- api/middlewares/usersMiddleware.js
+++ api/middlewares/usersMiddleware.js
@@ -8,7 +8,26 @@ async function hashPassword(req, res, next) {
   try {
     user.password = await bcrypt.hashSync(user.password, 12);
     req.user = user;
+    next();
   } catch (error) {
     next(new Error('Something went wrong. Please try again.'));
+  }
+}
+
+async function reversePasswordHash(req, res, next) {
+  const { username, password } = req.body;
+  try {
+    usersDb.findUserBy(username)
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          req.user = user;
+          next();
+        } else {
+          res.status(401).json({ message: 'Invalid credentials '});
+        }
+      })
+  } catch (error) {
+    next(error);
   }
 }
