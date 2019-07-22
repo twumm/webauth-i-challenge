31fbca6c74db6d47f82cd2a3e9a4234c514654af
Martin Twum Mensah <martint.mensah@gmail.com>
2019-07-22 12:07:37 +0000

    feature: Use helmet

--- api/server.js
+++ api/server.js
@@ -1,9 +1,11 @@
 /* eslint-disable no-console */
 const express = require('express');
 const cors = require('cors');
+const helmet = require('helmet');
 
 const server = express();
 
+server.use(helmet());
 server.use(express.json());
 server.use(cors());
 server.use(logger);
