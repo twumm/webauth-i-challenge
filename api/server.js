58365b6b76ac7c612bac8c09872e8d21580f7b06
Martin Twum Mensah <martint.mensah@gmail.com>
2019-07-22 12:05:52 +0000

    feature: Add logger and errorHandler Use cors on the server

--- api/server.js
+++ api/server.js
@@ -1,9 +1,12 @@
+/* eslint-disable no-console */
 const express = require('express');
 const cors = require('cors');
 
 const server = express();
 
 server.use(express.json());
+server.use(cors());
+server.use(logger);
 
 server.get('/', async (req, res, next) => {
   try {
@@ -11,6 +14,25 @@ server.get('/', async (req, res, next) => {
   } catch (error) {
     res.status(500).json({ message: "There was an error" });
   }
-})
+});
+
+function logger(req, res, next) {
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} request from ${req.url}}`
+  );
+  next();
+}
+
+function errorHandler(error, req, res) {
+  console.error('ERROR', error);
+  res
+    .status(500)
+    .json({
+      message: error.message,
+      stack: error.stack
+    });
+}
+
+server.use(errorHandler);
 
 module.exports = server;
