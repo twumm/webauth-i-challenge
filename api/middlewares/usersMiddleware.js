90ae2c711adaf06bebadb427c2066a48180232a1
Martin Twum Mensah <martint.mensah@gmail.com>
2019-07-22 13:39:49 +0000

    refactor: Hash and store password only

--- api/middlewares/usersMiddleware.js
+++ api/middlewares/usersMiddleware.js
@@ -4,10 +4,10 @@ const bcrypt = require('bcrypt');
 const usersDb = require('../users/usersDb');
 
 async function hashPassword(req, res, next) {
-  const user = { username, password } = req.body;
+  const { password } = req.body;
   try {
-    user.password = await bcrypt.hashSync(user.password, 12);
-    req.user = user;
+    const hashedPassword = await bcrypt.hashSync(password, 12);
+    req.hashedPassword = hashedPassword;
     next();
   } catch (error) {
     next(new Error('Something went wrong. Please try again.'));
