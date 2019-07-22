5b0d29c12cc391f3642252b19a08132e82802c83
Martin Twum Mensah <martint.mensah@gmail.com>
2019-07-22 13:03:09 +0000

    feature: Add methods to get all users and create new user

--- api/users/usersRouter.js
+++ api/users/usersRouter.js
@@ -12,4 +12,18 @@ router.get('/', async (req, res, next) => {
   } catch (error) {
     next(new Error('Could not retrieve users. Please try again later.'));
   }
+});
+
+router.post('/register', async (req, res, next) => {
+  const user = { username, password } = req.body;
+  try {
+    const newUser = await usersDb.addUser(user);
+    res
+      .status(201)
+      .json(newUser);
+  } catch (error) {
+    next(new Error('Could not register user. Please try again'));
+  }
 })
+
+module.exports = router;
