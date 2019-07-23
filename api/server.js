374cca37935eeea0555ea489848624c3c4fff2e8
Martin Twum Mensah <martint.mensah@gmail.com>
2019-07-23 11:35:11 +0000

    refactor: Use next() to send error message

--- api/server.js
+++ api/server.js
@@ -16,7 +16,7 @@ server.get('/', async (req, res, next) => {
   try {
     res.status(200).json('<h3>You got to the authentication app</h3>');
   } catch (error) {
-    res.status(500).json({ message: "There was an error" });
+    next(new Error('There was an error'));
   }
 });
 
