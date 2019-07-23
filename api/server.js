45f8f12d35e610eea32a174fb2e58ec279fe3649
Martin Twum Mensah <martint.mensah@gmail.com>
2019-07-23 11:54:11 +0000

    feature: Add express-session and knex session Configure knex session so it connects to the session table in db. This helps to prevent user being logged out on a server refresh

--- api/server.js
+++ api/server.js
@@ -2,12 +2,32 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+const session = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(session);
 
 const server = express();
 const usersRouter = require('../api/users/usersRouter');
 
 server.use(helmet());
 server.use(express.json());
+server.use(session({
+  name: 'hotInHereId',
+  secret: 'hsdf78y4rbjd9823rbd298ycb3498',
+  cookie: {
+    maxAge: 1000 * 60 * 60,
+    secure: false,
+    httpOnly: true,
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new KnexSessionStore({
+    knex: require('../data/dbConfig'), //configured instance of knex
+    tablename: 'user_sessions', // table that will store sessions inside the db, name can be anything
+    sidfieldname: 'sid', // column that will hold the session id, name can be anything
+    createtable: true, // if the table does not exist, it will create it automatically
+    clearInterval: 1000 * 60 * 60, // time it takes to check for old sessions and remove them from the database to keep it clean and performant
+  })
+}))
 server.use(cors());
 server.use(logger);
 server.use('/api/users', usersRouter);
